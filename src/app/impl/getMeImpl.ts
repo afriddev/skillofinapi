f82ea82239bee16e756054c6beea1b37a16a59e5
afriddev <afridayan01@gmail.com>
2025-02-19 18:10:33 +0530

    fix: remove unnecessary console log from getMe implementation

--- src/app/impl/getMeImpl.ts
+++ src/app/impl/getMeImpl.ts
@@ -31,7 +31,6 @@ export async function getMeIMPL(user: {
   const userAccountData = await userAccountModel.findOne({ emailId });
 
   const posts = await postModel.find().sort({ createdAt: -1 });
-  console.log(posts)
 
 
 

a1a4d21c71110cd320768209c82156909439eb79
afriddev <afridayan01@gmail.com>
2025-02-19 23:54:15 +0530

    fix: correct typo in database field name from postst to posts

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -34,7 +34,7 @@ export async function POST(req: Request) {
         { emailId },
         {
           $set: {
-            postst: myPosts,
+            posts: myPosts,
           },
         }
       );

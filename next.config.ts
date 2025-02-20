7d27d8184ca005c6a72921a45f91a4150de86cea
afriddev <afridayan01@gmail.com>
2025-02-20 13:14:03 +0530

    added localhost url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Applies to all API routes
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:5173", // Your React frontend URL
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

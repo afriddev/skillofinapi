7ab297c32899840b7b4253f0581010d10c1097a5
afriddev <afridayan01@gmail.com>
2025-02-23 23:07:21 +0530

    feat: implement payment processing logic in create payment route

--- src/app/api/createpayment/route.ts
+++ src/app/api/createpayment/route.ts
@@ -1,54 +1,54 @@
-// "use server";
+"use server";
 
-// import { exceptionEnums, responseEnums } from "@/app/enums/responseEnums";
-// import { NextResponse } from "next/server";
+import { exceptionEnums, responseEnums } from "@/app/enums/responseEnums";
+import { NextResponse } from "next/server";
 // import Stripe from "stripe";
 
 // const stripe = new Stripe("" as string, {
 //   apiVersion: "2025-01-27.acacia",
 // });
 
-// export async function POST(req: Request) {
-//   try {
-//     const request = await req.json();
+export async function POST(req: Request) {
+  try {
+    const request = await req.json();
 
-//     if (!request.authToken || !request.amount) {
-//       return NextResponse.json(
-//         { message: exceptionEnums.BAD_REQUEST },
-//         { status: 400 }
-//       );
-//     }
+    if (!request.authToken || !request.amount) {
+      return NextResponse.json(
+        { message: exceptionEnums.BAD_REQUEST },
+        { status: 400 }
+      );
+    }
 
-//     try {
-//       const paymentIntent = await stripe.paymentIntents.create({
-//         amount: parseInt(request?.amount) * 100,
-//         currency: "usd",
-//       });
+    try {
+      // const paymentIntent = await stripe.paymentIntents.create({
+      //   amount: parseInt(request?.amount) * 100,
+      //   currency: "usd",
+      // });
 
-//       return NextResponse.json(
-//         {
-//           message: responseEnums?.SUCCESS,
-//           clientSecret: paymentIntent?.client_secret,
-//         },
-//         {
-//           status: 200,
-//         }
-//       );
-//     } catch (e) {
-//       return NextResponse.json(
-//         {
-//           message: responseEnums?.ERROR,
-//         },
-//         {
-//           status: 200,
-//         }
-//       );
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     return NextResponse.json(
-//       { message: exceptionEnums.SERVER_ERROR },
-//       { status: 500 }
-//     );
-//   }
-// }
+      return NextResponse.json(
+        {
+          message: responseEnums?.SUCCESS,
+          clientSecret: "paymentIntent?.client_secret",
+        },
+        {
+          status: 200,
+        }
+      );
+    } catch (e) {
+      return NextResponse.json(
+        {
+          message: responseEnums?.ERROR,
+        },
+        {
+          status: 200,
+        }
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: exceptionEnums.SERVER_ERROR },
+      { status: 500 }
+    );
+  }
+}

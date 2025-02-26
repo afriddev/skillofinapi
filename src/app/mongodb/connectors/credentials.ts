dc511a8395e0f731a596ab558b7971ed1d37dde3
afriddev <afridayan01@gmail.com>
2025-02-26 23:29:36 +0530

    feat: use environment variable for MongoDB password in connection credentials

--- src/app/mongodb/connectors/credentials.ts
+++ src/app/mongodb/connectors/credentials.ts
@@ -1,4 +1,4 @@
-const password: string = "afriddev01";
+const password: string = process.env.MONGO_DB_PASSWORD as string;
 
 function getMongoURI(databaseName: string): string {
   return `mongodb+srv://afridayan01:${password}@cluster0.egh9y.mongodb.net/${databaseName}?retryWrites=true&w=majority&appName=Cluster0`;
